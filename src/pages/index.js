fe4f518ec3bbac1f94094f4a95c65279f95a3628
PhilippeMts <phi.metais@gmail.com>
2021-09-28 14:26:04 +0200

    ✏️ Fix typos.

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import SectionWhyHolium from '../components/sections/section-why-holium'
 import SectionSlideshow from '../components/sections/section-slideshow'
 import SectionTerminal from '../components/sections/section-terminal'
 import SectionAdditionalInformation
-  from '../components/sections/section-addtional-information'
+  from '../components/sections/section-additional-information'
 import Footer from '../components/sections/footer'
 import Banner from '../components/sections/banner'
 import SectionVideo from '../components/sections/section-video'
