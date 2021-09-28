6597ceeb3c3af69d03b6c2dc5cf8e704636af990
PhilippeMts <phi.metais@gmail.com>
2021-09-28 15:48:38 +0200

    💄 Update footer.

--- src/components/sections/footer.js
+++ src/components/sections/footer.js
@@ -3,38 +3,27 @@ import * as styles from './footer.module.scss'
 import classNames from 'classnames'
 import Typography from '../typography'
 import { StaticImage } from 'gatsby-plugin-image'
-import topSeparator from '../../images/separator-4.svg'
-import WavyWhiteLayout from '../layouts/wavy-white-layout'
 
 const Footer = () => {
   return (
-    <WavyWhiteLayout
-      topSeparator={topSeparator}
-      topSeparatorHeight={''}
-      topMargin={'16vw'}
-    >
       <div className={classNames('container', styles.wrapper)}>
         <div className={styles.column}>
-          <StaticImage src={'../../images/logo-dark.svg'} alt={'Holium logo'}
+          <StaticImage src={'../../images/logo-white.svg'} alt={'Holium logo'}
                        width={200}
                        placeholder={'none'}
                        className={styles.logo}
           />
         </div>
         <div className={styles.column}>
-          <Typography variant='h2'>Support</Typography>
-          <Typography variant='body3'>Documentation</Typography>
-          <Typography variant='body3'>Community</Typography>
-          <Typography variant='body3'>Contact Holium</Typography>
+          <Typography variant='body3'><i className={'ci-home_alt_fill'}/> Documentation</Typography>
+          <Typography variant='body3'><i className={'ci-discord'}/> Discord</Typography>
+          <Typography variant='body3'><i className={'ci-github'}/> Github</Typography>
         </div>
         <div className={styles.column}>
-          <Typography variant='h2'>Company</Typography>
           <Typography variant='body3'>About us</Typography>
-          <Typography variant='body3'>Blog</Typography>
           <Typography variant='body3'>Media kit</Typography>
         </div>
       </div>
-    </WavyWhiteLayout>
   )
 }
 
