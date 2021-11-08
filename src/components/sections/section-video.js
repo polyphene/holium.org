c6dfc1e28ab236e45ab70bb1ad1dd2f10e8dcbff
PhilippeMts <phi.metais@gmail.com>
2021-11-08 17:11:33 +0100

    🍱 Add video asset.

--- src/components/sections/section-video.js
+++ src/components/sections/section-video.js
@@ -1,16 +1,18 @@
 import * as React from 'react'
 import * as styles from './section-video.module.scss'
 import Frame from '../frame'
-import { StaticImage } from 'gatsby-plugin-image'
 
 const SectionVideo = () => {
   return (
     <div className={styles.wrapper}>
       <Frame>
-        <StaticImage src={'../../images/video-placeholder.svg'}
-                     alt={'video placeholder'}
-                     placeholder={'blurred'}
-        />
+        <div className={styles.iframeContainer}>
+          <iframe className={styles.responsiveIframe}
+                  src="https://www.youtube.com/embed/OlQwiaijBq4"
+                  title="Holium In 2 Minutes" frameBorder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen/>
+        </div>
       </Frame>
     </div>
   )
