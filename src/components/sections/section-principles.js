5b442cc816732665fc615a6da6a6d8fe7bdc01b5
PhilippeMts <phi.metais@gmail.com>
2021-11-09 07:02:05 +0100

    🍱 Add principle image assets.

--- src/components/sections/section-principles.js
+++ src/components/sections/section-principles.js
@@ -3,12 +3,15 @@ import * as styles from './section-principles.module.scss'
 import * as cardStyles from '../card.module.scss'
 import classNames from 'classnames'
 import Card from '../card'
-import { StaticImage } from 'gatsby-plugin-image'
 import FloatingSpheresWrapper, {
   BlueSphere,
   LightBlueSphere,
   PurpleSphere,
 } from '../layouts/floating-spheres-layout'
+import ImageTransform from '../../images/svg-assets/transform.svg'
+import ImageCollaborate from '../../images/svg-assets/collaborate.svg'
+import ImageIntegrate from '../../images/svg-assets/integrate.svg'
+import ImageRelax from '../../images/svg-assets/relax.svg'
 
 const SectionPrinciples = () => {
   return (
@@ -24,38 +27,22 @@ const SectionPrinciples = () => {
       <div className={classNames('container', styles.wrapper)}>
         <div className={styles.cardsWrapper}>
           <Card title={'Transform'} image={
-            <StaticImage src={'../../images/image-placeholder.svg'}
-                         alt={'placeholder'}
-                         placeholder={'blurred'}
-                         className={cardStyles.image}
-            />
+            <ImageTransform className={cardStyles.image}/>
           }>
             Transform structured, semi-structured and complex data.
           </Card>
           <Card title={'Collaborate'} image={
-            <StaticImage src={'../../images/image-placeholder.svg'}
-                         alt={'placeholder'}
-                         placeholder={'blurred'}
-                         className={cardStyles.image}
-            />
+            <ImageCollaborate className={cardStyles.image}/>
           }>
             Collaborate on pipelines to infuse trust into your data.
           </Card>
           <Card title={'Integrate'} image={
-            <StaticImage src={'../../images/image-placeholder.svg'}
-                         alt={'placeholder'}
-                         placeholder={'blurred'}
-                         className={cardStyles.image}
-            />
+            <ImageIntegrate className={cardStyles.image}/>
           }>
             Integrate into generic ETL and ELT flows.
           </Card>
           <Card title={'Relax'} image={
-            <StaticImage src={'../../images/image-placeholder.svg'}
-                         alt={'placeholder'}
-                         placeholder={'blurred'}
-                         className={cardStyles.image}
-            />
+            <ImageRelax className={cardStyles.image}/>
           }>
             Relax, while Web3 runs under the hood.
           </Card>
